857052dcbacc0bf56af4ced889aff11aef63f6f3
mirko tebaldi <mirkotebaldi@yahoo.it>
2013-09-25 00:35:50 +0200

    work in progress

--- jw_exec.py
+++ jw_exec.py
@@ -44,4 +44,50 @@ def showDailyText(language, date):
 	json_url = jw_config.const[language]["daily_text_json"] + "/" + date
 	print "JWORG daily text json " + json_url
 	json = jw_load.loadJsonFromUrl(json_url)
-	print json["items"][0]["content"].encode("utf8")
+	print json
+
+	mydisplay = MyClass()
+	mydisplay .doModal()
+	del mydisplay
+
+
+
+#get actioncodes from https://github.com/xbmc/xbmc/blob/master/xbmc/guilib/Key.h
+ACTION_PREVIOUS_MENU = 10
+ 
+class MyClass(xbmcgui.Window):
+  def __init__(self):
+    self.strActionInfo = xbmcgui.ControlLabel(100, 120, 200, 200, '', 'font13', '0xFFFF00FF')
+    self.addControl(self.strActionInfo)
+    self.strActionInfo.setLabel('Push BACK to quit')
+    self.button0 = xbmcgui.ControlButton(250, 100, 80, 30, "HELLO")
+    self.addControl(self.button0)
+    self.button1 = xbmcgui.ControlButton(250, 200, 80, 30, "HELLO2")
+    self.addControl(self.button1)
+    self.button2 = xbmcgui.ControlButton(450, 200, 80, 30, "HELLO3")
+    self.addControl(self.button2)
+    self.setFocus(self.button0)
+    self.button0.controlDown(self.button1)
+    self.button1.controlUp(self.button0)
+    self.button1.controlRight(self.button2)
+    self.button2.controlLeft(self.button1)
+ 
+  def onAction(self, action):
+    if action == ACTION_PREVIOUS_MENU:
+      self.close()
+ 
+  def onControl(self, control):
+    if control == self.button0:
+      self.message('you pushed the 1st button')
+    if control == self.button1:
+      self.message('you pushed the 2nd button')
+    if control == self.button2:
+      self.message('you pushed the 3rd button')
+ 
+  def message(self, message):
+    dialog = xbmcgui.Dialog()
+    dialog.ok(" My message title", message)
+ 
+mydisplay = MyClass()
+mydisplay .doModal()
+del mydisplay
